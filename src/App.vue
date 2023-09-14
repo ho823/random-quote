6fd3387673bf4b5203d269eb88a55668848eef14
ho823 <amelineh887@gmail.com>
2023-09-14 17:18:43 +0200

    add about page

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,24 @@ import { RouterLink, RouterView } from 'vue-router'
   <header>
     <div class="wrapper">
       <nav>
-        <RouterLink to="/">Quote</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">
+          <div class="link_word">
+            <span class="link_letter">Q</span>
+            <span class="link_letter">u</span>
+            <span class="link_letter">o</span>
+            <span class="link_letter">t</span>
+            <span class="link_letter">e</span>
+          </div>
+        </RouterLink>
+        <RouterLink to="/about">
+          <div class="link_word">
+            <span class="link_letter">A</span>
+            <span class="link_letter">b</span>
+            <span class="link_letter">o</span>
+            <span class="link_letter">u</span>
+            <span class="link_letter">t</span>
+          </div>
+        </RouterLink>
       </nav>
     </div>
   </header>
@@ -31,16 +47,8 @@ nav {
   text-align: center;
 }
 
-nav a.router-link-exact-active {
-  color: hsla(0, 0%, 24%, 0.861);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
 nav a {
-  display: inline-block;
+  display: inline-flex;
   padding: 0 1rem;
   border-left: 1px solid var(--color-border);
 }
@@ -48,6 +56,63 @@ nav a {
 nav a:first-of-type {
   border: 0;
 }
+
+
+@keyframes move {
+  from {
+    transform: translateY(0) translateZ(0);
+  }
+  to {
+    transform: translateY(1.2em) translateZ(0);
+    transition: transform .4s ease, background-color;
+  }
+}
+
+
+.link_word{
+  text-shadow: 0 calc(1.2em*-1) 0 rgb(6, 138, 138);
+  overflow: hidden;
+  display: flex;
+  transform: translateY(0) translateZ(0);
+  transition: transform .4s ease;
+  transition: color 2s;
+}
+
+.link_word:hover > * {
+  animation: move 1s;
+  animation-fill-mode: forwards;
+}
+
+.link_word:focus {
+  background-color: blue;
+}
+
+.link_letter {
+  backface-visibility: visible;
+  display: block;
+  font-style: normal;
+}
+
+.link_letter:first-child {
+  transition-delay: .05s;
+}
+
+.link_letter:nth-child(2) {
+  transition-delay: .1s;
+}
+
+.link_letter:nth-child(3) {
+  transition-delay: .15s;
+}
+
+.link_letter:nth-child(4) {
+  transition-delay: .2s;
+}
+
+.link_letter:nth-child(5) {
+  transition-delay: .25s;
+}
+
 
 @media (min-width: 1024px) {
   header {
---
+++ src/components/TheAbout.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="content-about">
+    <img id="img-team" src="@/assets/business-team.jpg" />
+    <div id="elm-decoration"></div>
+    <p id="text-about">Unde Rufinus ea tempestate praefectus praetorio ad discrimen trusus est ultimum. ire enim ipse compellebatur ad militem, 
+      quem exagitabat inopia simul et feritas, et alioqui coalito more.</p>
+  </div>
+  </template>
+  
+  <script setup lang="ts"> 
+  </script>
+  
+  <style>
+
+  #content-about {
+    height: 100vh;
+    width: 50rem;
+    padding: 5rem;
+    position: relative;
+  }
+
+  #elm-decoration {
+    width: 10rem;
+    height: 10rem;
+    background-color: aqua;
+    position: absolute;
+    top: 40rem;
+    right: 6rem;
+  }
+
+  #img-team {
+    height: 70vh;
+    width: 35rem;
+    object-fit: cover;
+  }
+
+  #text-about {
+    color: #FFFF;
+    background-color: rgba(23, 23, 23, 0.498);
+    padding: 1rem;
+    position: absolute;
+    z-index: 1000;
+    top: 20rem;
+    left: 11rem;
+    width: 45%;
+  }
+
+  </style>
+  
+  
